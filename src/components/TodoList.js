ae79955d08e3a7d17eb59d3e608b6a7b4f690d91
Bálint Csertán <csertanb@gmail.com>
2023-10-21 23:53:36 +0200

    add uuid npm package abel to add new todo with specific text

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,13 +1,37 @@
-import React from "react";
+import React, { useState } from "react";
+import { v4 as uuidv4 } from 'uuid';
 
 const TodoList = () => {
+    //in functional components we can use useState
+    const [ todos, setTodos ] = useState([
+        { text: 'Pay bills', id: uuidv4() },
+        { text: 'Do your homework', id: uuidv4() },
+        { text: 'Feed the dog', id: uuidv4() },
+    ]);
+
+    const addTodo = () => {
+        const newTodoInput = document.getElementById('newTodo');
+        if(newTodoInput.value !== ''){
+            setTodos([
+                ...todos,
+                { text: newTodoInput.value, id: uuidv4() }
+            ]);
+            newTodoInput.value = '';
+        }
+    }
+
     return (
         <div>
+            <form>
+                <input type="text" id="newTodo" />
+            </form>
+            <button onClick={addTodo}>Add a todo</button>
             <ul>
-                <li>Pay bills</li>
-                <li>Do your homework</li>
-                <li>Feed the dog</li>
+                {todos.map((todo) => 
+                    <li key={todo.id}>{todo.text}</li>
+                )}
             </ul>
+            
         </div>
     )
 }
