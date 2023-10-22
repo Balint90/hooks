6e11baffd61d347fa7220def3d09ee634d311cbf
Bálint Csertán <csertanb@gmail.com>
2023-10-22 11:29:42 +0200

    Add checkbox to todos (no function)

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -5,15 +5,15 @@ import { v4 as uuidv4 } from 'uuid';
 const TodoList = () => {
     //in functional components we can use useState
     const [ todos, setTodos ] = useState([
-        { text: 'Pay bills', id: uuidv4() },
-        { text: 'Do your homework', id: uuidv4() },
-        { text: 'Feed the dog', id: uuidv4() },
+        { text: 'Pay bills', id: uuidv4(), done: false },
+        { text: 'Do your homework', id: uuidv4(), done: false },
+        { text: 'Feed the dog', id: uuidv4(), done: false },
     ]);
 
     const addTodo = (text) => {
         setTodos([
             ...todos,
-            { text, id: uuidv4() }
+            { text, id: uuidv4(), done: false }
         ]);
     }
 
@@ -25,7 +25,14 @@ const TodoList = () => {
             <div class="col-12 col-md-6">
             <ul className="list-group">
                 {todos.map((todo) => 
-                    <li className="list-group-item" key={todo.id}>{todo.text}</li>
+                    <li className="list-group-item" key={todo.id}>
+                    <div className="form-check">
+                    <input type="checkbox" className="form-check-input" id={"todo" + todo.id} />
+                    <label class="form-check-label" for={"todo" + todo.id}>
+                        {todo.text}
+                    </label>
+                    </div>
+                    </li>
                 )}
             </ul>
             </div>
